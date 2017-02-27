1ceee8e3648d05d725bb26c38d280c3bbca6275f
Tamiko Terada <tamikodevo@gmail.com>
2017-02-27 11:41:46 -0800

    test added: Accounts#order_item returns Array

--- specs/restaurant_bill_spec.rb
+++ specs/restaurant_bill_spec.rb
@@ -2,3 +2,22 @@ require 'minitest/autorun'
 require 'minitest/reporters'
 require 'minitest/skip_dsl'
 require_relative '../lib/restaurant_bill'
+
+describe "RestaurantBill class" do
+  describe "Account#initialize" do
+    it "Creates a new instance of Restaurant Bill class" do
+      bill = RestaurantBill.new
+      bill.must_be_kind_of RestaurantBill
+    end
+  end
+
+  describe "Account#order_item" do
+    it "Returns an array" do
+      bill = RestaurantBill.new
+      item = "steak"
+      cost = 35
+      ordered = bill.order_item(item, cost)
+      ordered.must_be_kind_of Array
+    end
+  end
+end
